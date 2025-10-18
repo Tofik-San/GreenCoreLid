428fb964f701b92d58558f7b1506fb29bbe0d921
VeryHappyEpta <Tofkazlo@Gmai.com>
2025-10-18 09:07:21 +0300

    Обнова

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://web-production-310c7c7.up.railway.app/:path*", // прокси к API
+      },
+    ];
+  },
 };
 
 export default nextConfig;
